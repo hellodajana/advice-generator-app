7e86502820d9767d1761be6ffbce2b3594770091
hellodajana <dajanasmolyakov@gmail.com>
2022-07-03 19:25:47 +0200

    commented out console.log

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ const loadAvice = async () => {
   try {
     const API_LINK = "https://api.adviceslip.com/advice";
     const res = await fetch(API_LINK);
+    // console.log(res.ok);
     const data = await res.json();
-
+    // console.log(data);
     const title = document.getElementById("advice");
     const advice = document.getElementById("text");
     title.innerHTML = `Advice #${data.slip.id}`;
@@ -17,5 +18,3 @@ const btn = document.getElementById("btn");
 
 window.onload = loadAvice;
 btn.addEventListener("click", loadAvice);
-
-// doesn't work in firefox
