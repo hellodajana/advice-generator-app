cffad68b83b8803cee94378141385a7ae1bfeeda
hellodajana <dajanasmolyakov@gmail.com>
2022-07-04 13:30:38 +0200

    added cache: no cache

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const loadAvice = async () => {
   try {
     const API_LINK = "https://api.adviceslip.com/advice";
-    const res = await fetch(API_LINK);
+    const res = await fetch(API_LINK, { cache: "no-cache" });
     // console.log(res.ok);
     const data = await res.json();
-    // console.log(data);
+    const show = data.slip;
     const title = document.getElementById("advice");
     const advice = document.getElementById("text");
-    title.innerHTML = `Advice #${data.slip.id}`;
-    advice.innerHTML = `"${data.slip.advice}"`;
+    title.innerHTML = `Advice #${show.id}`;
+    advice.innerHTML = `"${show.advice}"`;
   } catch (error) {
     console.log(error);
   }
